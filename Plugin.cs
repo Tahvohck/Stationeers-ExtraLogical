c33978ba8351071d10b5718bca335d47d4035663
Tahvohck <Tahvohck@gmail.com>
2023-10-28 08:26:37 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,62 @@
+﻿using Assets.Scripts.Objects.Clothing;
+using Assets.Scripts.Objects.Entities;
+using Assets.Scripts.Objects.Items;
+using Assets.Scripts.Objects.Pipes;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ExtraLogical
+{
+    [BepInPlugin("ExtraLogical", "Extra Logical", "0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource sLogger;
+
+        private void Awake()
+        {
+            sLogger = Logger;
+            new Harmony("ExtraLogical").PatchAll(typeof(Patches));
+        }
+    }
+
+    public class Patches
+    {
+        [HarmonyPatch(typeof(AdvancedTablet), nameof(AdvancedTablet.GetLogicable))]
+        [HarmonyPatch(typeof(AdvancedSuit),   nameof(AdvancedSuit.GetLogicable))]
+        [HarmonyPostfix]
+        public static void GetLogicableWithCustomDevices (int deviceIndex, ref ILogicable __result, AdvancedTablet __instance)
+        {
+            if (__result != null) {
+                return;
+            }
+            Plugin.sLogger.LogInfo("Logic called. Index: [" + deviceIndex + "]");
+            Human H = __instance.RootParentHuman;
+            switch (deviceIndex) {
+                case 104: {
+                        __result = (H?.GlassesSlot.Occupant) as ILogicable;
+                        break;
+                    }
+                case 109: {
+                        __result = (H?.LeftHandSlot.Occupant) as ILogicable;
+                        break;
+                    }
+                case 110: {
+                        __result = (H?.RightHandSlot.Occupant) as ILogicable;
+                        break;
+                    }
+                default: {
+                        break;
+                    }
+            }
+        }
+
+        [HarmonyPatch(typeof(AdvancedTablet), nameof(AdvancedTablet.IsValidIndex))]
+        [HarmonyPatch(typeof(AdvancedSuit),   nameof(AdvancedSuit.IsValidIndex))]
+        [HarmonyPostfix]
+        public static void IsValidIndexWithCustomDevices(int index, ref bool __result)
+        {
+            __result = __result || index == 104;
+        }
+    }
+}
